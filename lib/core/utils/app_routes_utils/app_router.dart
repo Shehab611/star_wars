03cf756e9beb53081bca7179cc3c824fc9015f8f
sheha <shehabehab1029@gmail.com>
2024-07-12 00:51:11 +0300

    provide people cubit to the Home screen

--- lib/core/utils/app_routes_utils/app_router.dart
+++ lib/core/utils/app_routes_utils/app_router.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:star_wars/core/service_locator.dart';
+import 'package:star_wars/features/home/domain/use_cases/get_people_data.dart';
+import 'package:star_wars/features/home/presentation/view_model_manger/people_cubit/people_cubit.dart';
 import 'package:star_wars/features/home/presentation/views/home_screen.dart';
 
 part 'app_navigator.dart';
@@ -6,6 +10,9 @@ part 'app_paths.dart';
 
 abstract final class AppRouter {
   static final Map<String, WidgetBuilder> routes = {
-    AppPathName.kHomeScreen: (BuildContext context) => const HomeScreen()
+    AppPathName.kHomeScreen: (BuildContext context) => BlocProvider(
+          create: (context) => PeopleCubit(GetPeopleData(sl.get()))..getData(),
+          child: const HomeScreen(),
+        )
   };
 }
